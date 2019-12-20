145ae76fa48c4e639116b873ebb3164dbe7e2a07
kokole130 <kokole130@gmail.com>
2019-12-20 16:08:37 +0200

    few more changes in dgraph class

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -4,64 +4,72 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 public class DGraph implements graph{
-	static int keyNum=0;
+	static int keyNum;
 	LinkedList<Vertex> ver;
-	LinkedList<LinkedList<Edge>> edge;//the outer linkedlist represent the source id by indexes, and the inner linkedlist represent all the edges with the same source id
-	String edges;
-	int EdgeSize;
+	LinkedList<Edge> edge;
+	//String edges;
+	//int EdgeSize;
 	int mc;
-	
+
 	public DGraph() {
 		this.ver=new LinkedList<>();
-		this.edges="";
-		
-		this.EdgeSize=0;
+		this.edge=new LinkedList<>();
+		this.keyNum=1;
 		this.mc=0;
 	}
 	
-	@Override
+	private int hashCode(int src,int dest) {
+		
+		return 0;//index in the edges list
+	}
+
+	@Override//@@@@@@@@@@@@@@
 	public node_data getNode(int key) {
 		if(key<this.ver.size()) {
-			return this.ver.get(key);
+			if(key==this.ver.get(key-1).key) {
+				return this.ver.get(key-1);
+			}
+			else return null;
 		}
 		else {
 			return null;
 		}
 	}
 
-	@Override
+	@Override//@@@@@@@@@@@@@@
 	public edge_data getEdge(int src, int dest) {
-		if(this.edges.contains("s"+src+"d"+dest)) {
-			double weight=checkWeight(src, dest);
-			Edge data=new Edge(this.ver.get(src), this.ver.get(dest), weight);
-			return data;
-		}
-		else {
-			return null;
-		}
+//		if(this.edges.contains("s"+src+"d"+dest)) {
+//			double weight=checkWeight(src, dest);
+//			Edge data=new Edge(this.ver.get(src), this.ver.get(dest), weight);
+//			return data;
+//		}
+//		else {
+//			return null;
+//		}
+		return null;
 	}
 
-	
-	private double checkWeight(int src,int dest) {//beside function
-		String temp="s"+src+"d"+dest;
-		int index=this.edges.indexOf(temp);//to get to the beginning of the edge
-		index=index+4;//to get 'w' position
-		String temp2=this.edges.substring(index+1);
-		int after=temp2.indexOf(',');
-		double weight=Double.parseDouble(temp2.substring(index+1,after));
-		
-		return weight;
-	}
-	
+//
+//	private double checkWeight(int src,int dest) {//beside function
+//		String temp="s"+src+"d"+dest;
+//		int index=this.edges.indexOf(temp);//to get to the beginning of the edge
+//		index=index+4;//to get 'w' position
+//		String temp2=this.edges.substring(index+1);
+//		int after=temp2.indexOf(',');
+//		double weight=Double.parseDouble(temp2.substring(index+1,after));
+//
+//		return weight;
+//	}
+
 	@Override
 	public void addNode(node_data n) {
-		
+
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -90,20 +98,17 @@ public class DGraph implements graph{
 
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.ver.size();
 	}
 
 	@Override
 	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.edge.size();
 	}
 
 	@Override
 	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.mc;
 	}
 
 }
